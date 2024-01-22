2b999e7a46056b9c3cd39f3dcf2faf2fcae0af00
wtgjohnson <wtgjohnson@gmail.com>
2024-01-21 21:40:07 -0800

    completed tutorial

--- main.gd
+++ main.gd
@@ -14,13 +14,22 @@ func _process(delta):
 
 
 func game_over():
+	#print("game over")
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
+	#print("here")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_mob_timer_timeout():
@@ -51,6 +60,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
